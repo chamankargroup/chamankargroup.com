55dec2af32a3f498957e0d22de9403fbe9baba0e
SarveshMina <105467665+SarveshMina@users.noreply.github.com>
2026-02-01 01:01:32 +0000

    Fix Supabase API key

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://apljvusmsrcjsthufuvp.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFwbGp2dXNtc3JjanN0aHVmdXZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgzMzQyNjIsImV4cCI6MjA1MzkxMDI2Mn0.Ay93JDegJ-L8gz2QLsNm8m3pxXNFLO0dgnUhvzIGS5s'
+const supabaseAnonKey = 'sb_publishable__Bv3F7XYEELALffXv8MwAg_H30pjBAg'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
